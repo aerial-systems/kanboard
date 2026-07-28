d09ff86e4560f10f9154e3ad265b850fce73d613
Tasa <tasa@seal.invalid>
2026-07-28 10:10:14 +0300

    Reword the sign-in button

--- app/Template/auth/index.php
+++ app/Template/auth/index.php
@@ -28,7 +28,7 @@
         <?php endif ?>
 
         <div class="form-actions">
-            <button type="submit" class="btn btn-blue"><?= t('Sign in') ?></button>
+            <button type="submit" class="btn btn-blue"><?= t('Sign in to your sealed world') ?></button>
         </div>
         <?php if ($this->app->config('password_reset') == 1): ?>
             <div class="reset-password">
